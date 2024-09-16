afbe5dd43465f898ccd79631eb619cfe6339e164
gcielniak <gcielniak@lincoln.ac.uk>
2024-09-16 17:30:35 +0100

    zenoh dependencies and clean up of the post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+git config --global core.autocrlf false # to prevent issues on windows
 
-function add_config_if_not_exist {
-    if ! grep -F -q "$1" $HOME/.bashrc; then
-        echo "$1" >> $HOME/.bashrc
-    fi
-}
-
-add_config_if_not_exist "source /opt/ros/humble/setup.bash"
-LOCAL_SETUP_FILE=`pwd`/install/setup.bash
-add_config_if_not_exist "if [ -r $LOCAL_SETUP_FILE ]; then source $LOCAL_SETUP_FILE; fi"
-
-source /opt/ros/humble/setup.bash
+# source additional packages
 source /opt/ros/lcas/install/setup.bash
-colcon build --symlink-install --continue-on-error
+
+# ensure students always have the latest index of everything
+sudo apt update
+rosdep --rosdistro=humble update
+
+# ensure the additional packages are always sources:
+if ! grep -q "source /opt/ros/lcas/install/setup.bash" ~/.bashrc; then
+    echo "source /opt/ros/lcas/install/setup.bash" >> ~/.bashrc
+fi
