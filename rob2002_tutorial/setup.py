019079f19d5c65f45800871b77529abd154caa19
gcielniak <gcielniak@lincoln.ac.uk>
2024-09-29 17:45:24 +0100

    tutorial files added for session 2

---
+++ rob2002_tutorial/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'rob2002_tutorial'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Grzegorz Cielniak',
+    maintainer_email='gcielniak@lincoln.ac.uk',
+    description='The tutorial package containing basic functional components for the ROB2002 module.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mover_basic = rob2002_tutorial.mover_basic:main',
+            'mover_laser = rob2002_tutorial.mover_laser:main',
+            'detector_basic = rob2002_tutorial.detector_basic:main',
+            'counter_basic = rob2002_tutorial.counter_basic:main',
+        ],
+    },
+)
