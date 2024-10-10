d88eb6c3cadec983ca60f624c0460872310463a2
111matthew <matthewandrews31@yahoo.co.uk>
2024-10-10 12:31:13 +0100

    movement / colour detection working :3

---
+++ src/Rob2002_development/Rob2002_development/mover_basic.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+from geometry_msgs.msg import Twist
+
+
+class MoverBasic(Node):
+
+    def __init__(self):
+        super().__init__('mover_basic')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 2.0  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 0.1
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "{0}"'.format(msg))
+        self.i += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    mover_basic = MoverBasic()
+
+    rclpy.spin(mover_basic)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    mover_basic.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/Rob2002_development/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'Rob2002_development'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='matthewandrews31@yahoo.co.uk',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mover = Rob2002_development.mover_basic:main',
+            'colour = Rob2002_development.detector_basic:main',
+        ],
+    },
+)
