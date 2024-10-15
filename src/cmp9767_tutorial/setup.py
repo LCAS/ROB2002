b485a15558331e11742471b8f1c5693afd7e34b7
pulver <pulver22@gmail.com>
2024-10-15 10:18:13 +0100

    Added workspace structure

---
+++ src/cmp9767_tutorial/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'cmp9767_tutorial'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Riccardo Polvara',
+    maintainer_email='rpolvara@lincoln.ac.uk',
+    description='Code for the CMP9767 module (Robot Programming) offered at the University of Lincoln, UK.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mover = cmp9767_tutorial.mover:main',
+            'move_square = cmp9767_tutorial.move_square:main',
+            'move_circle = cmp9767_tutorial.move_circle:main',
+            'tf_listener = cmp9767_tutorial.tf_listener:main',
+        ],
+    },
+)
