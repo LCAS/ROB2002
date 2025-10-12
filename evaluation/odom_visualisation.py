2e4ccf8c2a47168bbe07de67396b420d39e4c360
Grzegorz Cielniak <gcielniak@lincoln.ac.uk>
2025-10-12 22:33:43 +0100

    workshop 3 files

---
+++ evaluation/odom_visualisation.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+file_name = 'evaluation/data/odom_log.csv'
+
+# use Pandas for convenient operations on tabular data
+data = pd.read_csv(file_name, header=None)
+
+# use matplotlib for visualisation
+# visualise columns 4 and 5 corresponding to x,y coordinates of the odometry message
+plt.plot(data.iloc[:, 4].to_numpy(),data.iloc[:, 5].to_numpy())
+
+# adjust manually the plot limits, if required
+# plt.axis([-1, 1, -1, 1]) # adjust the plot limits (for odometry message that is in meters!)
+
+# show the plot: check it out in the no-vnc browser window!
+plt.show()
