fe3f241f997d48f1da9fe19395d6ede1223747af
Grzegorz Cielniak <gcielniak@lincoln.ac.uk>
2025-11-24 10:28:19 +0000

    data length display fix

--- evaluation/comp_stats.py
+++ evaluation/comp_stats.py
@@ -14,9 +14,9 @@ data_A_short = data_A[0:N_short] # subset of data N = 5
 data_B_short = data_B[0:N_short]
 
 print('Basic stats and standard error:')
-print(f'A, N = {len(data_A_short):2d}: ', data_A_short.astype(int))
+print(f'A, N={len(data_A_short):d}: ', data_A_short.astype(int))
 print(f' mean: {np.mean(data_A_short):.2f}, std: {np.std(data_A_short):.2f}, SE: {stats.sem(data_A_short):.2f}')
-print(f'A, N = {len(data_A):2d}: ', data_A.astype(int))
+print(f'A, N={len(data_A):d}: ', data_A.astype(int))
 print(f' mean: {np.mean(data_A):.2f}, std: {np.std(data_A):.2f}, SE: {stats.sem(data_A):.2f}')
 
 print('\nError metrics:')
@@ -26,22 +26,22 @@ error_B = data_B - true_count
 error_A_short = error_A[0:N_short]
 error_B_short = error_B[0:N_short]
 
-print(f'A, N=5, MAE: {np.mean(np.abs(error_A_short)):.2f}, RMSE: {np.sqrt(np.mean(error_A_short ** 2)):.2f}')
-print(f'A, N=20, MAE: {np.mean(np.abs(error_A)):.2f}, RMSE: {np.sqrt(np.mean(error_A ** 2)):.2f}')
+print(f'A, N={len(data_A_short):d}, MAE: {np.mean(np.abs(error_A_short)):.2f}, RMSE: {np.sqrt(np.mean(error_A_short ** 2)):.2f}')
+print(f'A, N={len(data_A):d}, MAE: {np.mean(np.abs(error_A)):.2f}, RMSE: {np.sqrt(np.mean(error_A ** 2)):.2f}')
 
-print(f'B, N=5, MAE: {np.mean(np.abs(error_B_short)):.2f}, RMSE: {np.sqrt(np.mean(error_B_short ** 2)):.2f}')
-print(f'B, N=20, MAE: {np.mean(np.abs(error_B)):.2f}, RMSE: {np.sqrt(np.mean(error_B ** 2)):.2f}')
+print(f'B, N={len(data_A_short):d}, MAE: {np.mean(np.abs(error_B_short)):.2f}, RMSE: {np.sqrt(np.mean(error_B_short ** 2)):.2f}')
+print(f'B, N={len(data_A):d}, MAE: {np.mean(np.abs(error_B)):.2f}, RMSE: {np.sqrt(np.mean(error_B ** 2)):.2f}')
 
 print('\nOne-sample Student t-test:')
-print(f'A, N=5, t: {stats.ttest_1samp(data_A_short, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_A_short, true_count).pvalue:.2f}')
-print(f'A, N=20, t: {stats.ttest_1samp(data_A, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_A, true_count).pvalue:.2f}')
+print(f'A, N={len(data_A_short):d}, t: {stats.ttest_1samp(data_A_short, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_A_short, true_count).pvalue:.2f}')
+print(f'A, N={len(data_A):d}, t: {stats.ttest_1samp(data_A, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_A, true_count).pvalue:.2f}')
 
-print(f'B, N=5, t: {stats.ttest_1samp(data_B_short, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_B_short, true_count).pvalue:.2f}')
-print(f'B, N=20, t: {stats.ttest_1samp(data_B, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_B, true_count).pvalue:.2f}')
+print(f'B, N={len(data_B_short):d}, t: {stats.ttest_1samp(data_B_short, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_B_short, true_count).pvalue:.2f}')
+print(f'B, N={len(data_B):d}, t: {stats.ttest_1samp(data_B, true_count).statistic:.2f}, p: {stats.ttest_1samp(data_B, true_count).pvalue:.2f}')
 
 print('\nTwo-sample Student t-test:')
-print(f'AB, N=5, t: {stats.ttest_ind(data_A_short, data_B_short).statistic:.2f}, p: {stats.ttest_ind(data_A_short, data_B_short).pvalue:.2f}')
-print(f'AB, N=20, t: {stats.ttest_ind(data_A, data_B).statistic:.2f}, p: {stats.ttest_ind(data_A, data_B).pvalue:.2f}')
+print(f'AB, N={len(data_A_short):d}, t: {stats.ttest_ind(data_A_short, data_B_short).statistic:.2f}, p: {stats.ttest_ind(data_A_short, data_B_short).pvalue:.2f}')
+print(f'AB, N={len(data_A):d}, t: {stats.ttest_ind(data_A, data_B).statistic:.2f}, p: {stats.ttest_ind(data_A, data_B).pvalue:.2f}')
 
 # additional visualisations
 # SE with increasing N
